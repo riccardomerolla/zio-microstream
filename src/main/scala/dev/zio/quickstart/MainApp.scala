064eb4e6bc52c44d6a04280899d69316c1a0e23b
Riccardo Merolla <riccardo.merolla@hey.com>
2022-11-02 18:28:04 +0100

    wip

--- src/main/scala/dev/zio/quickstart/MainApp.scala
+++ src/main/scala/dev/zio/quickstart/MainApp.scala
@@ -9,19 +9,24 @@ import java.util.Date
 final class MicroStream(private val store: EmbeddedStorageManager) extends AnyVal:
 
   def access[A](f: EmbeddedStorageManager => A): ZIO[Any, Throwable, A] = ZIO.attemptBlocking(f(store))
+
   private def getStore =
     access(_)
-  def setRoot[A](newRoot: A) =
-    access(_.setRoot(newRoot))
 
-  def storeRoot() =
+  def setRoot[A](newRoot: A): ZIO[Any, Throwable, A] =
+    access(_.setRoot(newRoot).asInstanceOf[A])
+
+  def storeRoot(): ZIO[Any, Throwable, Long] =
     access(_.storeRoot())
 
-  def root() =
-    access(_.root())
+  def root[A](): ZIO[Any, Throwable, A] =
+    access(_.root().asInstanceOf[A])
 
-  def shutdown() =
+  def shutdown(): ZIO[Any, Throwable, Boolean] =
     access(_.shutdown())
+
+  def isActive(): ZIO[Any, Throwable, Boolean] =
+    access(_.isActive())
 
 object MicroStream:
   def make = ZIO.succeed(MicroStream(EmbeddedStorage.start))
@@ -31,10 +36,11 @@ object MainApp extends ZIOAppDefault {
   //def run = Console.printLine("Hello, World!")
   def run = for {
     m <- MicroStream.make
-    _ <- m.root().debug
-    _ <- m.setRoot("Hello World! @ " + new Date())
-    _ <- m.storeRoot()
-    _ <- m.root().debug
-    _ <- m.shutdown()
+    _ <- m.root[String]().debug
+    _ <- m.setRoot[String]("Hello World! @ " + new Date())
+    _ <- m.storeRoot().debug
+    _ <- m.root[String]().debug
+    _ <- m.shutdown().debug
+    _ <- m.isActive().debug
   } yield ()
 }
